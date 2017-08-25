3ab569690462151b24e1eb3bb7449d6b8fb34e38
Vincent Riesop <Vincent.Riesop@gmail.com>
2017-08-25 13:25:37 +0200

    Keep Annotations at runtime

--- src/main/java/bp/common/model/annotations/EditableAttribute.java
+++ src/main/java/bp/common/model/annotations/EditableAttribute.java
@@ -3,6 +3,7 @@ package bp.common.model.annotations;
 /**
  * Created by Vincent on 27.06.2017.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EditableAttribute {
     String value();
 }
