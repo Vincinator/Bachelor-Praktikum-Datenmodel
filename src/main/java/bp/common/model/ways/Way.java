5320d2fe06a12d14f53d776729af6425eaeb7f3c
Bi Banh Bao <skisssbb@hotmail.de>
2017-09-06 23:03:19 +0200

    Debug

--- src/main/java/bp/common/model/ways/Way.java
+++ src/main/java/bp/common/model/ways/Way.java
@@ -57,7 +57,7 @@ public class Way {
         this.name = name;
     }
 
-    @OneToMany
+    @OneToMany(cascade = {CascadeType.ALL})
     public List<Node> getNodes() {
         return nodes;
     }
