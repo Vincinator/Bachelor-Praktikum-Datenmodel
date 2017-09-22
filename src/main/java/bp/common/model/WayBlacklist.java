85a6caa74e8e0a8c907e0501aa7284bd3131d594
Bi Banh Bao <skisssbb@hotmail.de>
2017-09-23 00:13:49 +0200

    Update WayBlacklist Class

---
+++ src/main/java/bp/common/model/WayBlacklist.java
@@ -0,0 +1,18 @@
+package bp.common.model;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import javax.persistence.*;
+
+/**
+ * Created by Bi Banh Bao on 23.09.2017.
+ */
+@Entity
+@Table(name = "WAYS")
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+public class WayBlacklist {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public long id;
+    private long osm_id;
+}
